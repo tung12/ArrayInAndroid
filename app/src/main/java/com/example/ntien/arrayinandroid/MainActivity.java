63118937b89230bd570dfff7d2c8d2a6e54aff34
Nghiêm Tiến Tùng <ntientung01@gmail.com>
2017-10-15 18:06:29 +0700

    complete List View

--- app/src/main/java/com/example/ntien/arrayinandroid/MainActivity.java
+++ app/src/main/java/com/example/ntien/arrayinandroid/MainActivity.java
@@ -14,50 +14,27 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ListView l;
-    ArrayList<String> arrayList;
-    EditText editText;
-    Button btnAdd;
+    ArrayList<TraiCay> arrayList;
+    TraiCayAdapter traiCayAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AnhXa();
+        traiCayAdapter = new TraiCayAdapter(this,R.layout.dong_trai_cay,arrayList);
+        l.setAdapter(traiCayAdapter);
+
+    }
+
+    public  void  AnhXa(){
         l=(ListView) findViewById(R.id.listView);
         arrayList = new ArrayList<>();
-        editText = (EditText) findViewById(R.id.txtInsert);
-        btnAdd = (Button) findViewById(R.id.btnAdd);
 
-        arrayList.add("Android");
-        arrayList.add("IOS");
-        arrayList.add("PHP");
-        arrayList.add("Unity");
-        arrayList.add("Java");
-        arrayList.add("C");
-
-        final ArrayAdapter adapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1,arrayList);
-
-        l.setAdapter(adapter);
-
-        l.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this,arrayList.get(position),Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!editText.getText().toString().isEmpty()){
-                    arrayList.add(editText.getText().toString());
-                    adapter.notifyDataSetChanged();
-                    Toast.makeText(MainActivity.this,"Added", Toast.LENGTH_SHORT);
-
-                }
-
-                else{
-                    Toast.makeText(MainActivity.this,"Not Empty", Toast.LENGTH_SHORT);
-                }
-            }
-        });
+        arrayList.add(new TraiCay("Dâu Tây","Dâu Tây Đà Lạt",0));
+        arrayList.add(new TraiCay("Dâu Tây","Dâu Tây Đà Lạt",0));
+        arrayList.add(new TraiCay("Dâu Tây","Dâu Tây Đà Lạt",0));
+        arrayList.add(new TraiCay("Dâu Tây","Dâu Tây Đà Lạt",0));
+        arrayList.add(new TraiCay("Dâu Tây","Dâu Tây Đà Lạt",0));
     }
 }
